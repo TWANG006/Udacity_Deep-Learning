7350eb393eb742d0ad5422d53c1aeab13d60bf97
TWANG006 <twang006@e.ntu.edu.sg>
2016-12-05 17:05:55 +0800

    Ass1, problem 1&2 done

--- Udacity_Deep-Learning_Python-Codes/Assignment1/Assignment1.py
+++ Udacity_Deep-Learning_Python-Codes/Assignment1/Assignment1.py
@@ -1,10 +1,10 @@
 from __future__ import print_function
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 import numpy as np
 import os
 import sys
 import tarfile
-from IPython.display import display, Image
 from scipy import ndimage
 from sklearn.linear_model import LogisticRegression
 from urllib.request import urlretrieve
@@ -81,3 +81,61 @@ def maybe_extract(filename, force = False):
 
 train_folders = maybe_extract(train_filename)
 test_folders = maybe_extract(test_filename)
+
+#Problem 1 Display images
+#image_files = os.listdir(train_folders[0])
+
+#img = mpimg.imread(os.path.join(train_folders[0], image_files[0]))
+#plt.imshow(img,cmap='gray')
+#plt.show()
+
+image_size = 28     # image width&height
+pixel_depth = 255.0 # max intensity
+
+def load_letter(folder, min_num_images):
+    # load the data for a single letter
+    image_files = os.listdir(folder)
+    dataset = np.ndarray(shape=(len(image_files), image_size, image_size),dype=np.float32)
+    print(folder)    
+    num_images = 0
+
+    for img in image_files:
+        image_file = os.path.join(folder, image)
+        try:
+            image_data = (ndimage.imread(image_file).astype(float) - pixel_depth/2)/pixel_depth
+            if image_data.shape != (image_size, image_size):
+                raise Exception('Unexpected image shape: %s' % str(image_data.shape))
+            dataset[num_images, :, :] = image_data
+            num_images = num_images + 1
+        except IOError as e:
+            print('Could not read: ', image_file, ':', e, ' - it\'s ok, skipping.')
+
+    dataset = dataset[0:num_images, :, :]   #rescale the dataset to the correct size
+    if num_images < min_num_images:
+        raise Exception('Many fewer images than expected: %d < %d' %
+                        (num_images, min_num_images))
+
+    print('Full dataset tensor: ', dataset.shape)
+    print('Mean:', np.mean(dataset))
+    print('Standard deviation:', np.std(dataset))
+    return dataset
+
+def maybe_pickle(data_folders, min_num_images_per_class, force = False):
+    dataset_names = []
+    for folder in data_folders:
+        set_filename = folder + '.pickle'
+        dataset_names.append(set_filename)
+        if os.path.exists(set_filename) and not force:
+            print('%s already present - Skipping pickling.' % set_filename)
+        else:
+            print('Pickling %s.' % set_filename)
+            dataset = load_letter(folder, min_num_images_per_class)
+            try:
+                with open(set_filename, 'wb') as f:
+                    pickle.dump(dataset, f, pickle.HIGHEST_PROTOCOL)
+            except Exception as e:
+                print('Unable to save data to ', set_filename, ':', e)
+    return dataset_names
+
+
+
