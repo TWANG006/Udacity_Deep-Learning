b19fa3de6f54d8cea9f2e5d5bf69052cf58ad204
TWANG006 <twang006@e.ntu.edu.sg>
2016-12-09 13:25:32 +0800

    Assignment 1 done

--- Udacity_Deep-Learning_Python-Codes/Assignment1/Assignment1.py
+++ Udacity_Deep-Learning_Python-Codes/Assignment1/Assignment1.py
@@ -6,6 +6,8 @@ import numpy as np
 import os
 import sys
 import tarfile
+import hashlib
+import time
 from scipy import ndimage
 from sklearn.linear_model import LogisticRegression
 from urllib.request import urlretrieve
@@ -191,60 +193,60 @@ def disp_number_images(datasets):
 disp_number_images(train_datasets)
 disp_number_images(test_datasets)
 
-def make_array(nb_rows, img_size):
-    if nb_rows:
-        dataset = np.ndarray((nb_rows, img_size, img_size), dtype = np.float32)
-        labels = np.ndarray(nb_rows, dtype = np.int32)
-    else:
-        dataset, labels = None, None
-    return dataset, labels
+def make_arrays(nb_rows, img_size):
+  if nb_rows:
+    dataset = np.ndarray((nb_rows, img_size, img_size), dtype=np.float32)
+    labels = np.ndarray(nb_rows, dtype=np.int32)
+  else:
+    dataset, labels = None, None
+  return dataset, labels
 
-def merge_datasets(pickle_files, train_size, valid_size = 0):
-    num_classes = len(pickle_files)
-    valid_dataset, valid_labels = make_array(valid_size, image_size)
-    train_dataset, train_labels = make_array(train_size, image_size)
-    vsize_per_class = valid_size // num_classes
-    tsize_per_class = train_size // num_classes
-
-    start_v, start_t = 0, 0
-    end_v, end_t = vsize_per_class, tsize_per_class
-    end_l = vsize_per_class + tsize_per_class
-
-    for label, pickle_file in enumerate(pickle_files):
-        try:
-            with open(pickle_file,'rb') as f:
-                letter_set = pickle.load(f)
-                # let's shuffle the letters to have random validation and training set
-                np.random.shuffle(letter_set)
-                if valid_dataset is not None:
-                    valid_letter = letter_set[:vsize_per_class,:,:]
-                    valid_dataset[start_v:end_v, :, :] = valid_letter
-                    valid_labels[start_v:end_v] = label
-                    start_v += vsize_per_class
-                    end_v += vsize_per_class
-
-                train_letter = letter_set[vsize_per_class:end_l,:,:]
-                train_dataset[start_t:end_t,:,:] = train_letter
-                train_labels[start_t:end_t] = label
-                start_t += tsize_per_class
-                end_t += tsize_per_class
-        except Exception as e:
-            print('Unable to process data from', pickle_file, ':', e)
-            raise
+def merge_datasets(pickle_files, train_size, valid_size=0):
+  num_classes = len(pickle_files)
+  valid_dataset, valid_labels = make_arrays(valid_size, image_size)
+  train_dataset, train_labels = make_arrays(train_size, image_size)
+  vsize_per_class = valid_size // num_classes
+  tsize_per_class = train_size // num_classes
     
-    return valid_dataset, valid_labels, train_dataset, train_labels
-
+  start_v, start_t = 0, 0
+  end_v, end_t = vsize_per_class, tsize_per_class
+  end_l = vsize_per_class+tsize_per_class
+  for label, pickle_file in enumerate(pickle_files):       
+    try:
+      with open(pickle_file, 'rb') as f:
+        letter_set = pickle.load(f)
+        # let's shuffle the letters to have random validation and training set
+        np.random.shuffle(letter_set)
+        if valid_dataset is not None:
+          valid_letter = letter_set[:vsize_per_class, :, :]
+          valid_dataset[start_v:end_v, :, :] = valid_letter
+          valid_labels[start_v:end_v] = label
+          start_v += vsize_per_class
+          end_v += vsize_per_class
+                    
+        train_letter = letter_set[vsize_per_class:end_l, :, :]
+        train_dataset[start_t:end_t, :, :] = train_letter
+        train_labels[start_t:end_t] = label
+        start_t += tsize_per_class
+        end_t += tsize_per_class
+    except Exception as e:
+      print('Unable to process data from', pickle_file, ':', e)
+      raise
+    
+  return valid_dataset, valid_labels, train_dataset, train_labels
+            
+            
 train_size = 200000
 valid_size = 10000
 test_size = 10000
 
 valid_dataset, valid_labels, train_dataset, train_labels = merge_datasets(
-    train_datasets, train_size, valid_size)
-_,_,test_dataset, test_labels = merge_datasets(test_datasets, test_size)
+  train_datasets, train_size, valid_size)
+_, _, test_dataset, test_labels = merge_datasets(test_datasets, test_size)
 
-print('Training', train_dataset.shape, train_labels.shape)
-print('Validation', valid_dataset.shape, valid_labels.shape)
-print('Testing', test_dataset.shape, test_labels.shape)
+print('Training:', train_dataset.shape, train_labels.shape)
+print('Validation:', valid_dataset.shape, valid_labels.shape)
+print('Testing:', test_dataset.shape, test_labels.shape)
 
 def randomize(dataset, labels):
     permutation = np.random.permutation(labels.shape[0])
@@ -291,7 +293,87 @@ except Exception as e:
     print('Unable to save data to', pickle_file, ':',e)
     raise
 
-statinfo = os.stat(pickle_file)
-print('Compressed pickle size:', statinfo.st_size)
+    statinfo = os.stat(pickle_file)
+    print('Compressed pickle size:', statinfo.st_size)
 
 #Problem 5 Sanetize
+def display_overlap(overlap, source_dataset, target_dataset):
+    item = random.choice(list(overlap.keys()))
+    imgs = np.concatenate(([source_dataset[item]], target_dataset[overlap[item][0:7]]))
+    plt.suptitle(item)
+    for i, img in enumerate(imgs):
+        plt.subplot(2, 4, i+1)
+        plt.axis('off')
+        plt.imshow(img)
+    plt.show()
+
+def sanetize(dataset_1, dataset_2, labels_1):
+    dataset_hash_1 = np.array([hashlib.sha256(img).hexdigest() for img in dataset_1])
+    dataset_hash_2 = np.array([hashlib.sha256(img).hexdigest() for img in dataset_2])
+    overlap = [] # list of indexes
+    for i, hash1 in enumerate(dataset_hash_1):
+        duplicates = np.where(dataset_hash_2 == hash1)
+        if len(duplicates[0]):
+            overlap.append(i) 
+    return np.delete(dataset_1, overlap, 0), np.delete(labels_1, overlap, None)
+
+test_dataset_sanit, test_labels_sanit = sanetize(test_dataset, train_dataset, test_labels)
+print('Overlapping images removed: ', len(test_dataset) - len(test_dataset_sanit))
+valid_dataset_sanit, valid_labels_sanit = sanetize(valid_dataset, train_dataset, valid_labels)
+print('Overlapping images removed: ', len(valid_dataset) - len(valid_dataset_sanit))
+
+pickle_file_sanit = dataPath + 'notMNIST_sanit.pickle'
+
+try:
+    f = open(pickle_file_sanit, 'wb')
+    save = {
+         'train_dataset': train_dataset,
+         'train_labels': train_labels,
+         'valid_dataset': valid_dataset_sanit,
+         'valid_labels': valid_labels_sanit,
+         'test_dataset': test_dataset_sanit,
+         'test_labels': test_labels_sanit,
+         }
+    pickle.dump(save, f, pickle.HIGHEST_PROTOCOL)
+    f.close()
+except Exception as e:
+    print('Unable to save data to ', pickle_file_sanit, ':', e)
+    raise
+statinfo = os.stat(pickle_file_sanit)
+print('Compressed pickle size: ', statinfo.st_size)
+
+#Problem 6 Trin the data
+regr = LogisticRegression()
+X_test = test_dataset.reshape(test_dataset.shape[0], 28*28)
+Y_test = test_labels
+
+sample_size = 50
+X_train = train_dataset[:sample_size].reshape(sample_size, 28*28)
+Y_train = train_labels[:sample_size]
+regr.fit(X_train, Y_train)
+regr.score(X_test, Y_test)
+pred_labels = regr.predict(X_test)
+dis_shuffled_dataset(test_dataset, pred_labels)
+
+sample_size = 1000
+X_train = train_dataset[:sample_size].reshape(sample_size, 28*28)
+Y_train = train_labels[:sample_size]
+regr.fit(X_train, Y_train)
+regr.score(X_test, Y_test)
+
+sample_size = 5000
+X_train = train_dataset[:sample_size].reshape(sample_size, 28*28)
+Y_train = train_labels[:sample_size]
+regr.fit(X_train, Y_train)
+regr.score(X_test, Y_test)
+
+regr2 = LogisticRegression(solver = 'sag')
+sample_size = len(train_dataset)
+X_train = train_dataset[:sample_size].reshape(sample_size, 784)
+Y_train = train_labels[:sample_size]
+regr2.fit(X_train, Y_train)
+regr2.score(X_test, Y_test)
+
+
+pred_labels = regr.predict(X_test)
+dis_shuffled_dataset(test_dataset, pred_labels)
